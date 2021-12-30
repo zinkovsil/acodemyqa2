f1242d5255229a50a0776dbb094f4e3a6b4efc6a
Ilja <zinkovsilja@gmail.com>
2021-12-30 15:38:12 +0200

    [feature/create_readme]: HomeWork class save

---
+++ src/main/java/HomeWork/HomeWork.java
@@ -0,0 +1,53 @@
+package HomeWork;
+
+public class HomeWork {
+    public static void main(String[] args) {
+
+        double earthWeight = 5.972E24;
+        System.out.println(earthWeight);
+        System.out.println();
+
+        float earthOrbitalPeriod = 365.256363004F;
+        System.out.println(earthOrbitalPeriod);
+
+        int earthSurfaceArea = 510065623;
+        System.out.println(earthSurfaceArea);
+
+        long usaDebtAmount = 29481248350000L;
+        System.out.println(usaDebtAmount);
+
+
+        short latvianArmyPersonal = 7400;
+        System.out.println(latvianArmyPersonal);
+
+        byte membersOfLatvianParlament = 100;
+        System.out.println(membersOfLatvianParlament);
+        System.out.println(100);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+
+        byte currentTemperatureInLatvia = -5;
+        System.out.println(currentTemperatureInLatvia);
+
+        char millitaryPersonalInIceland = '1';
+        System.out.println(millitaryPersonalInIceland);
+
+        char firstAlphaBetLetter = 'A';
+        System.out.println(firstAlphaBetLetter);
+        System.out.println("Character Max Value: "+(Character.MAX_VALUE+0));
+        System.out.println("Character Min Value: "+(Character.MIN_VALUE+0));
+
+        boolean latviaIsACountry = true;
+        System.out.println(latviaIsACountry);
+
+
+
+
+
+
+        
+
+
+
+    }
+}
