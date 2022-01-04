180ec5d74b8ad43f63d40469da98490a2f949aef
zinkovsil <95440072+zinkovsil@users.noreply.github.com>
2022-01-04 16:05:43 +0200

    Feature/java1 (#5)
    * [feature/create_readme]: HomeWork class creation
    
    * [feature/create_readme]: HomeWork class save
    
    * [feature/create_readme]: HomeWork class save (MIN/MAX vaue added).
    
    * [feature/create_readme]: Homework update. 3,4 is done.
    
    * [feature/create_readme]: Homework update. 3,4 is done.
    
    * [feature/create_readme]: Homework update. 3,4,5 is done.
    
    * [feature/create_readme]: Homework update. 3,4,5 is done.
    
    * [feature/create_readme]: Homework update. 3,4,5 is done.
    
    * [feature/create_readme]: Homework update. Finishing 1 exercise
    
    * [feature/create_readme]: Homework update. Finishing 1 exercise
    
    * [feature/create_readme]: Homework update. Finished my Homework

---
+++ src/main/java/HomeWork/HomeWork.java
@@ -0,0 +1,297 @@
+package HomeWork;
+
+public class HomeWork {
+    public static void main(String[] args) {
+//      Exrcise Nr.1 and Nr.2
+
+        double earthWeight = 5.972E24D;
+        System.out.println(earthWeight);
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
+
+        double earthAge = 4.543E9D;
+        System.out.println(earthAge);
+
+        double earthSurfaceGravity = 9.80665D;
+        System.out.println(earthSurfaceGravity);
+
+        double earthSiderialRotationPeriod = 0.99726968D;
+        System.out.println(earthSiderialRotationPeriod);
+
+        double earthEquatorialRotationVelocity = 0.4651D;
+        System.out.println(earthEquatorialRotationVelocity);
+
+        double earthEccentricity = 0.0167086D;
+        System.out.println(earthEccentricity);
+
+        double marsEccentricity = 0.0934D;
+        System.out.println(marsEccentricity);
+
+        double marsWeight = 6.39E23D;
+        System.out.println(marsWeight);
+
+        double veneraWeight = 4.867E24D;
+        System.out.println(veneraWeight);
+
+        double iHaveNoIdea = - 99.9999999D;
+        System.out.println(iHaveNoIdea);
+
+
+
+//      float
+
+        float earthOrbitalPeriod = 365.25F;
+        System.out.println(earthOrbitalPeriod);
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+
+        float appleStock = 182.01F;
+        System.out.println(appleStock);
+
+        float highestMountainOnEarth = 8.48F;
+        System.out.println(highestMountainOnEarth);
+
+        float pNumber = 3.14F;
+        System.out.println(pNumber);
+
+        float UsainBoltOlympicRecord = 9.58F;
+        System.out.println(UsainBoltOlympicRecord);
+
+        float LebronJamesHeight = 2.06F;
+        System.out.println(LebronJamesHeight);
+
+        float MichaelJordanAveragePointsPerGame = 30.1F;
+        System.out.println(MichaelJordanAveragePointsPerGame);
+
+        float MichaelJordanHeight = 1.98F;
+        System.out.println(MichaelJordanHeight);
+
+        float xiaomiStock = 18.56F;
+        System.out.println(xiaomiStock);
+
+        float speedOfLightFromSunTillEarth = 1.08F;
+        System.out.println(speedOfLightFromSunTillEarth);
+
+//      int
+
+        int earthSurfaceArea = 510065623;
+        System.out.println(earthSurfaceArea);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+
+        int peopleLivingInPoverty = 689000000;
+        System.out.println(peopleLivingInPoverty);
+
+        int chinaArmyPersonal = 2185000;
+        System.out.println(chinaArmyPersonal);
+
+        int wsopWinnerPrize = 8000000;
+        System.out.println(wsopWinnerPrize);
+
+        int alexanderOvechkinSalary = 9538462;
+        System.out.println(alexanderOvechkinSalary);
+
+        int latvianDefenceBudget = 758350000;
+        System.out.println(latvianDefenceBudget);
+
+        int TheFirstHumanAppeared = 2000000;
+        System.out.println(TheFirstHumanAppeared);
+
+        int arnisRieksinsWorth = 172000000;
+        System.out.println(arnisRieksinsWorth);
+
+        int mostExpensiveFlatInTheWorld = 200000000;
+        System.out.println(mostExpensiveFlatInTheWorld);
+
+        int michaelJordanNetWorth = 1600000000;
+        System.out.println(michaelJordanNetWorth);
+
+//      long
+
+        long usaDebtAmount = 29481248350000L;
+        System.out.println(usaDebtAmount);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+
+        long totalWorldPopulation =  7874965825L;
+        System.out.println(totalWorldPopulation);
+
+        long totalWorldGDP = 80934771028340L;
+        System.out.println(totalWorldGDP);
+
+        long appleCapitalization = 2986000000000L;
+        System.out.println(appleCapitalization);
+
+        long chinaPopulation = 1411778724L;
+        System.out.println(chinaPopulation);
+
+        long jeffBezosNetWorth = 202000000000L;
+        System.out.println(jeffBezosNetWorth);
+
+        long budgetOfLatvia = 10660000000L;
+        System.out.println(budgetOfLatvia);
+
+        long russiaDefenceBudget = 61700000000L;
+        System.out.println(russiaDefenceBudget);
+
+        long totalWorldDebt = 226000000000L;
+        System.out.println(totalWorldDebt);
+
+        long googleRevenue = 65000000000L;
+        System.out.println(googleRevenue);
+
+//      short
+
+        short latvianArmyPersonal = 7400;
+        System.out.println(latvianArmyPersonal);
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+
+        short lowestShortNumber = - 32768;
+        System.out.println(lowestShortNumber);
+
+        short VaticanPopulation = 836;
+        System.out.println(VaticanPopulation);
+
+        short RezeknePopulation = 27820;
+        System.out.println(RezeknePopulation);
+
+        short michaelJordanTotalNBAPoints = 32292;
+        System.out.println(michaelJordanTotalNBAPoints);
+
+        short numberOfVacanciesOnCVLV = 2963;
+        System.out.println(numberOfVacanciesOnCVLV);
+
+        short zasulauksPopulation = 7358;
+        System.out.println(zasulauksPopulation);
+
+        short distanceBetweenRigaAndMoscow = 842;
+        System.out.println(distanceBetweenRigaAndMoscow);
+
+        short numberOfEmployeesInRigasSatiksme = 3800;
+        System.out.println(numberOfEmployeesInRigasSatiksme);
+
+        short averageNetSalaryInLatviaGross = 1200;
+        System.out.println(averageNetSalaryInLatviaGross);
+
+//      byte
+
+        byte membersOfLatvianParlament = 100;
+        System.out.println(membersOfLatvianParlament);
+        System.out.println(100);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+
+        byte currentTemperatureInLatvia = -5;
+        System.out.println(currentTemperatureInLatvia);
+
+        byte numberOfStudentsInAcodemyQA2 = 20;
+        System.out.println(numberOfStudentsInAcodemyQA2);
+
+        byte numberOfApartmentsInMyHouse = 16;
+        System.out.println(numberOfApartmentsInMyHouse);
+
+        byte myFavoriteNumber = 8;
+        System.out.println(myFavoriteNumber);
+
+        byte numberOfMonthsInAYear = 12;
+        System.out.println(numberOfMonthsInAYear);
+
+        byte numberOfDaysInJanuary = 31;
+        System.out.println(numberOfDaysInJanuary);
+
+        byte numberOfFullWeeksInJanuary = 4;
+        System.out.println(numberOfFullWeeksInJanuary);
+
+        byte myAge = 30;
+        System.out.println(myAge);
+
+        byte lowestByteNumber = -128;
+        System.out.println(lowestByteNumber);
+
+
+
+
+
+
+        char millitaryPersonalInIceland = '1';
+        System.out.println(millitaryPersonalInIceland);
+
+        char firstAlphaBetLetter = 'A';
+        System.out.println(firstAlphaBetLetter);
+        System.out.println("Character Max Value: "+(Character.MAX_VALUE+0));
+        System.out.println("Character Min Value: "+(Character.MIN_VALUE+0));
+
+        char lastAlphabetLetter = 'Z';
+        System.out.println(lastAlphabetLetter);
+
+        char firstLetterOfMyName = 'I';
+        System.out.println(firstLetterOfMyName);
+
+        char numberOfPresidentsInLatvija = '1';
+        System.out.println(numberOfPresidentsInLatvija);
+
+        char myFamileMembers = '4';
+        System.out.println(myFamileMembers);
+
+        char balticStatesCountry = '3';
+        System.out.println(balticStatesCountry);
+
+        char firstLetterOfMySurname = 'Z';
+        System.out.println(firstLetterOfMySurname);
+
+        char firstLetterOfMyDadName = 'A';
+        System.out.println(firstLetterOfMyDadName);
+
+        char myFavoriteLetter = 'P';
+        System.out.println(myFavoriteLetter);
+
+        boolean latviaIsACountry = true;
+        System.out.println(latviaIsACountry);
+
+//      Exercise nr. 3
+
+        int a = 5;
+        int b = 2+8;
+        int d = 15/3;
+        System.out.println(a+b);
+        System.out.println(a+d);
+        System.out.println(a-b);
+        System.out.println(b-d);
+        System.out.println(a*b);
+        System.out.println(b*d);
+        System.out.println(a/d);
+        System.out.println(b/d);
+        System.out.println(d/b);
+        System.out.println((a+b)/d);
+        System.out.println(a%b);
+        System.out.println(d%b);
+
+//      Exercise nr. 4
+
+        String myFavoriteFootballTeam = "AJAX";
+        String myFatherName = "Andrej";
+        String myItTeacherName = "Nikita";
+        String myBrotherName = "Alexey";
+        String myLocalGymName = "Myfitness";
+        System.out.println(myFavoriteFootballTeam);
+        System.out.println(myFatherName);
+        System.out.println(myItTeacherName);
+        System.out.println(myBrotherName);
+        System.out.println(myLocalGymName);
+
+//      Exercise nr. 5
+
+        String city = "Riga";
+        String status = "is";
+        String word = "a";
+        String location = "capital";
+        String continent = "city";
+        String country = "of Latvia";
+        String population = "with total population six hundred thousand people.";
+        System.out.println(city +" "+ status +" "+ word +" "+ location +" "+ continent +" "+ country +" "+ population);
+
+
+
+    }
+}
