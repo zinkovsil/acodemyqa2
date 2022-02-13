3a28acb84d158f04bb622eb99de12d44e741cbc7
Ilja <zinkovsilja@gmail.com>
2022-02-13 16:07:57 +0200

    [feature/create_readme]: Homework number 3 (exсept calculator)

---
+++ src/main/java/Classwork/ArrayPractce.java
@@ -0,0 +1,9 @@
+package Classwork;
+
+public class ArrayPractce
+
+{
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/HomeWork/HomeWork.java
@@ -0,0 +1,69 @@
+package HomeWork;
+
+public class HomeWork {
+    public static void main(String[] args) {
+        // exercise 3
+        String[] animals = {"monkey", "lion", "tiger", "dog", "horse", "eagle", "camel", "chicken", "shark", "cow"};
+
+        for (int i = 0; i < animals.length; i++) {
+            System.out.println(animals[0]);
+        }
+
+        // exercise 6
+
+        for (int i = 1; i <= 100; i++)
+            System.out.println(i);
+
+        // exercise 7
+
+        for (int i = 50; i >= 0; i--)
+            System.out.println(i);
+
+        // exercise 4
+
+        for (int i = 1; i <= 10; i++)
+            if (i % 2 == 0)
+                System.out.println(i);
+
+        // exrecise 1
+
+        for (int i = 10; i <= 10; i++)
+            System.out.println("Integer value is 10");
+
+        // exercise 2
+
+        int Age = 1;
+        if (Age >= 18 && Age <= 21) {
+            System.out.println("Your discount is 10%");
+        } else if (Age >= 22 && Age <= 65) {
+            System.out.println("Your discount is 15%");
+        } else if (Age >= 65) {
+            System.out.println("Your discount is 20%");
+        } else {
+            System.out.println("No discount");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+
